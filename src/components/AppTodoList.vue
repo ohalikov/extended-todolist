e12f7918a1438a21af7894d677e724bc7f1293ce
ohalikov <olegway31@gmail.com>
2023-10-14 21:42:37 +0500

    WIP: divide on components, del boilerplate code from AppTodoList

---
+++ src/components/AppTodoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="todo-list">
+    <AppTodoItem
+      v-for="(todo, index) in todos"
+      :key="index"
+      :todo="{
+        id: index,
+        text: todo.text,
+        completed: todo.completed,
+      }"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppTodoItem from './AppTodoItem.vue';
+export default defineComponent({
+  components: {
+    AppTodoItem,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+          text: 'Learn the basics of Vue',
+          completed: true,
+        },
+        {
+          id: 1,
+          text: 'Learn the basics of Typescript',
+          completed: false,
+        },
+        {
+          id: 2,
+          text: 'Subscribe to the channel',
+          completed: false,
+        },
+        {
+          id: 3,
+          text: 'Learn the basics of JS',
+          completed: true,
+        },
+      ],
+    };
+  },
+});
+</script>
